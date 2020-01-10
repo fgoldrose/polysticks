b600ff3c6d93b604f2a15eba68475e2ec7bde8b0
Fred Goldstein-Rose <goldsteinrose@cs.uchicago.edu>
2020-01-10 16:26:20 -0600

    changed to have only one graph, but havent fixed enumerate function yet

--- polysticks.py
+++ polysticks.py
@@ -1,39 +1,42 @@
 import copy
+import networkx as nx
+import matplotlib.pyplot as plt
 
-n = 4
+n = 2
 
-vertgraph = {}
-horizgraph = {}
-#finalvgraph = {}
+graph = {}
 
-start_state = {"partitions":[None]*n, "horiz":[], "vert":[]}
+start_state = [None]*n
 
-def state_name(state):
+def state_name(state, vertical):
 	name = []
-	for i in state["partitions"]:
+	if vertical:
+		name.append("V")
+	else:
+		name.append("H")
+	for i in state:
 		if i == None:
-			name.append("N")
+			name.append(None)
 		else:
-			nn = ""
+			nn = []
 			for j in sorted(i):
-				nn += str(j)
-			name.append(nn)
+				nn.append(j)
+			name.append(tuple(sorted(i)))
 	return tuple(name)
 
-#vert is currently broken bc doesnt take into account that you can 
-# get to an outcome in several ways. needs to be a weighted graph.
-def vert(state):
-	if state_name(state) in vertgraph:
-		return
 
-	state_copy = {"partitions":copy.deepcopy(state["partitions"]), "horiz":[], "vert":[]}
-	state["vert"].append(state_copy)
+# returns a list of states vertically accessible from state
+def vert(state):
+	state_copy = copy.deepcopy(state)
+	vert_results = []
+	vert_results.append(state_copy)
+
 	for i in range(1, n):
-		temp = copy.deepcopy(state["vert"])
-		for inter_state in state["vert"]:
-			c = inter_state["partitions"]
-			clone_state = {"partitions":copy.deepcopy(c), "horiz":[], "vert":[]}
-			d = clone_state["partitions"]
+		temp = copy.deepcopy(vert_results)
+		for inter_state in vert_results:
+			c = inter_state
+			clone_state = copy.deepcopy(c)
+			d = clone_state
 			if c[i-1] == None and c[i] == None:
 				newlist = [i-1, i]
 				d[i-1] = newlist
@@ -50,117 +53,21 @@ def vert(state):
 					d[b] = concat
 			#if they are the same, an extra copy is added
 			temp.append(clone_state)
-		state["vert"] = temp
+		vert_results = temp
+	return vert_results
 
-	name = state_name(state)
-	vertgraph[name] = {}
-	for v in state["vert"]:
-		sn = state_name(v)
-		if sn in vertgraph[name]:
-			vertgraph[name][sn] +=1
-		else:
-			vertgraph[name][sn] = 1
-
-'''
-def finalvert(state, notop, nobottom):
-	name = state_name(state)
-	if name in finalvgraph:
-		if notop and nobottom and "-tb" in finalvgraph[name]:
-				return
-		if notop and "-t" in finalvgraph[name]:
-			return
-		if nobottom and "-b" in finalvgraph[name]:
-			return
-		if not (notop or nobottom) and "+tb" in finalvgraph[name]:
-			return
-
-	finalverts = []
-	state_copy = {"partitions":copy.deepcopy(state["partitions"]), "horiz":[], "vert":[]}
-	finalverts.append(state_copy)
-	start = 1
-	end = n
-	c = state_copy["partitions"]
-	if notop and c[0] == None:
-		start = 2
-		if c[1] == None:
-			newlist = [0, 1]
-			c[0] = newlist
-			c[1] = newlist
-		else:
-			c[0] = c[1]
-			c[1].append(0)
-
-	if nobottom and c[n-1] == None:
-		end = n-1
-		if c[n-2] == None:
-			newlist = [n-2, n-1]
-			c[n-2] = newlist
-			c[n-1] = newlist
-		else:
-			c[n-1] = c[n-2]
-			c[n-2].append(n-1)
-
-	toremove = []
-	for i in range(start, end):
-		temp = copy.deepcopy(finalverts)
-		for inter_state in finalverts:
-			c = inter_state["partitions"]
-			clone_state = {"partitions":copy.deepcopy(c), "horiz":[], "vert":[]}
-			d = clone_state["partitions"]
-			if c[i-1] == None and c[i] == None:
-				newlist = [i-1, i]
-				d[i-1] = newlist
-				d[i] = newlist
-			elif c[i] == None:
-				d[i] = d[i-1]
-				d[i-1].append(i)
-				#if c[i:] != [None]*(n-i):
-				#	temp.remove(inter_state)
-			elif c[i-1] == None:
-				d[i-1] = d[i]
-				d[i].append(i-1)
-			elif c[i-1] != c[i]: #dont want to add a new one, must join them.
-				concat = d[i-1] + d[i]
-				for b in concat:
-					d[b] = concat
-				toremove.append(inter_state)
-			temp.append(clone_state)
-		finalverts = temp
-	for r in toremove:
-		if r in finalverts:
-			finalverts.remove(r)
-
-	addon = "+tb"
-	if nobottom and notop:
-		addon = "-tb"
-	elif notop:
-		addon = "-t"
-	elif nobottom:
-		addon = "-b"
-
-	if name not in finalvgraph:
-		finalvgraph[name] = {}
-	finalvgraph[name][addon] = {}
-	for v in finalverts:
-		sn = state_name(v)
-		if sn in finalvgraph[name][addon]:
-			finalvgraph[name][addon][sn] +=1
-		else:
-			finalvgraph[name][addon][sn] = 1
-'''
 
 def horiz(state):
-	if state_name(state) in horizgraph:
-		return
+	state_copy = copy.deepcopy(state)
+	horiz_results = []
+	horiz_results.append(state_copy)
 
-	state_copy = {"partitions":copy.deepcopy(state["partitions"]), "horiz":[], "vert":[]}
-	state["horiz"].append(state_copy)
 	for i in range(0, n):
-		temp = copy.deepcopy(state["horiz"])
-		for inter_state in state["horiz"]:
-			c = inter_state["partitions"]
-			clone_state = {"partitions":copy.deepcopy(c), "horiz":[], "vert":[]}
-			d = clone_state["partitions"]
+		temp = copy.deepcopy(horiz_results)
+		for inter_state in horiz_results:
+			c = inter_state
+			clone_state = copy.deepcopy(c)
+			d = clone_state
 			if c[i] == None:
 				d[i] = [i]
 			elif len(d[i]) > 1:
@@ -169,40 +76,83 @@ def horiz(state):
 			else:
 				continue
 			temp.append(clone_state)
-		state["horiz"] = temp
+		horiz_results = temp
 
 	# delete [None]*n from the list since it is not valid (but was needed as a helper)
-	for p in state["horiz"]:
-		if p["partitions"] == [None]*n:
-			state["horiz"].remove(p)
+	for p in horiz_results:
+		if p == [None]*n:
+			horiz_results.remove(p)
+	return horiz_results
 
-	horizgraph[state_name(state)] = []
-	for h in state["horiz"]:
-		sn = state_name(h)
-		horizgraph[state_name(state)].append(sn)
+def name_to_state(name):
+	state = []
+	for p in name:
+		if p == None:
+			state.append(None)
+		elif p != "V" and p != "H":
+			if list(p) not in state:
+				state.append(list(p))
+			else:
+				state.append(state[state.index(list(p))])
+
+	return state
 
 def generate_graphs(start):
-	vert(start)
+	name = state_name(start, True)
+	graph[name] = {}
+	vert_results = vert(start)
+	for v in vert_results:
+		sn = state_name(v, False)
+		if sn in graph[name]:
+			graph[name][sn]['weight'] +=1
+		else:
+			graph[name][sn] = {'weight': 1}
+	graph[('END')] = {}
 
-	'''
-	no_t = False
-	no_b = False
-	if start["partitions"][0] == None:
-		no_t = True
-	if start["partitions"][n-1] == None:
-		no_b = True
-	finalvert(start, False, False)
-	if no_t:
-		finalvert(start, True, False)
-	if no_b:
-		finalvert(start, False, True)
-	if no_t and no_b:
-		finalvert(start, True, True)
-	'''
-	for s in start["vert"]:
-		horiz(s)
-		for t in s["horiz"]:
-			generate_graphs(t)
+	allfound = False
+	while allfound == False:
+		allfound = True
+		graph_updates = {}
+
+		for state in graph:
+			for s in graph[state]:
+				if s not in graph:
+					allfound = False
+					graph_updates[s] = {}
+					if s[0] == "H":
+						if valid_end(s):
+							graph_updates[s]['END'] = {'weight': 1}					
+						newstate = name_to_state(s)					
+						horiz_results = horiz(newstate)
+						for h in horiz_results:
+							sn = state_name(h, True)
+							graph_updates[s][sn] = {'weight': 1}
+					elif s[0] == "V":
+						newstate = name_to_state(s)
+						vert_results = vert(newstate)
+						for v in vert_results:
+							sn = state_name(v, False)
+							if sn in graph_updates[s]:
+								graph_updates[s][sn]['weight'] +=1
+							else:
+								graph_updates[s][sn] = {'weight': 1}
+					else:
+						print("sanity check")
+						print(s[0])
+		graph.update(graph_updates)
+
+#checks if there are multiple groups
+def valid_end(name):
+	s = set(name)
+	if None in s:
+		s.remove(None)
+	if 'V' in s:
+		return False
+	if 'H' in s:
+		s.remove('H')
+	if len(s) != 1:
+		return False
+	return True
 
 #checks if there are multiple groups
 def invalid_end(name):
@@ -214,6 +164,7 @@ def invalid_end(name):
 	return False
 
 #this can be way more efficient!
+#not yet updated for new method
 def enumerate(width, start, vert, top, bottom):
 	acc = 0
 	if width == 0:
@@ -242,22 +193,25 @@ def enumerate(width, start, vert, top, bottom):
 			acc += enumerate(width, h, True, t, b)
 	return acc
 
-ll = [1,2]
-test_state = {"partitions":[None, ll, ll], "horiz":[], "vert":[]}
-#test_state = start_state
-#print(test_state)
-#vert(test_state)
-#horiz(test_state)
-#print(test_state)
+
 generate_graphs(start_state)
-print("vertgraph", vertgraph)
-print("horizgraph", horizgraph)
+print("graph")
+for v in graph:
+	print(v, graph[v])
+G = nx.DiGraph(graph)
+A = nx.adjacency_matrix(G, weight='weight')
+print(A.todense())
+#nx.draw(G, with_labels=False)
+#plt.show()
 
-print("enum ", enumerate(1, ('N',)*n, True, False, False))
-print("enum ", enumerate(2, ('N',)*n, True, False, False))
-print("enum ", enumerate(3, ('N',)*n, True, False, False))
-print("enum ", enumerate(4, ('N',)*n, True, False, False))
-print("enum ", enumerate(5, ('N',)*n, True, False, False))
-print("enum ", enumerate(6, ('N',)*n, True, False, False))
 
+'''
+print("enum ", enumerate(1, (None,)*n, True, False, False))
+print("enum ", enumerate(2, (None,)*n,  True, False, False))
+print("enum ", enumerate(3, (None,)*n,  True, False, False))
+print("enum ", enumerate(4, (None,)*n,  True, False, False))
+print("enum ", enumerate(5, (None,)*n,  True, False, False))
+print("enum ", enumerate(6, (None,)*n,  True, False, False))
+print("enum ", enumerate(7, (None,)*n,  True, False, False))
+'''
 
